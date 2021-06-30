2286814fbddbfccbc7197c811da4aecedc728cb9
QuLuQ <you@example.com>
2021-06-30 17:40:25 +0500

    Salomati is our new PAGE

--- HelloWorld/Controllers/HomeController.cs
+++ HelloWorld/Controllers/HomeController.cs
@@ -27,7 +27,11 @@ namespace HelloWorld.Controllers
         {
             return View();
         }
-
+        public IActionResult Salomati()
+        {
+            return View() ;
+        }
+        
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
