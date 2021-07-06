b7b122b5eefe592d67aaa944d347c7eaca713bcc
Murtazoev <86365931+Murtazoev@users.noreply.github.com>
2021-07-07 00:06:51 +0500

    Bugging

--- HelloWorld/Controllers/StudentController.cs
+++ HelloWorld/Controllers/StudentController.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HelloWorld.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HelloWorld.Controllers
 {
     public class StudentController : Controller
     {
         // GET
+        private readonly StudService _studService;
+
+        public StudentController()
+        {
+            _studService = new StudService();
+        }
         public IActionResult Index()
         {
             return View();
@@ -15,9 +22,12 @@ namespace HelloWorld.Controllers
             return View();
         }
 
-        public IActionResult add()
+        public IActionResult add(Stud student)
         {
-            return View();
+            if (ModelState.IsValid == false)
+                return View(student);
+            var adding = _studService.NewStudent(student);
+            return RedirectToAction("list");
         }
     }
 }
--- HelloWorld/Models/StudService/StudService.cs
+++ HelloWorld/Models/StudService/StudService.cs
@@ -1,4 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using JsonConverter = System.Text.Json.Serialization.JsonConverter;
+
 namespace HelloWorld.Models.ViewModel
 {
     public class StudService
@@ -9,8 +16,20 @@ namespace HelloWorld.Models.ViewModel
         }
         public List<Stud> GetListOfStud()
         {
-            var newlist = new List<Stud>();
-            return newlist;
+            var newlist = File.ReadAllText("student.json");
+            var studeent = JsonConvert.DeserializeObject<List<Stud>>(newlist);
+            return studeent;
+        }
+
+        public Stud NewStudent(Stud newstudent)
+        {
+            var everyone = GetListOfStud();
+            var min = everyone.OrderBy(x => x.id).Last();
+            newstudent.id = min.id + 1 ;
+            everyone.Add(newstudent);
+            var text = JsonConvert.SerializeObject(everyone);
+            System.IO.File.WriteAllText("student.json", text);
+            return newstudent;
         }
     }
 }
