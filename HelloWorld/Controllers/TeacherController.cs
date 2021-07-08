54d0e40c2adefde4cbc6c4936bb9794d8f3d5e16
Abdurahmonzoda <A.Safarkhon@gmail.com>
2021-07-08 13:13:37 +0500

    Added Teachers service

---
+++ HelloWorld/Controllers/TeacherController.cs
@@ -0,0 +1,28 @@
+﻿using HelloWorld.Models.Services.TeacherService;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+
+namespace HelloWorld.Controllers
+{
+    public class TeacherController:Controller
+    {
+        private readonly TeacherService _teacherService;
+        public TeacherController()
+        {
+            _teacherService = new TeacherService();
+        }
+
+
+        public IActionResult Index()
+        {
+            var data = _teacherService.GetTechers();
+            return View(data);
+        }
+        
+        public string Test()
+        {
+            var data = _teacherService.GetSimpeInfo();
+            return data;
+        }
+    }
+}
---
+++ HelloWorld/Models/ViewModel/Teacher.cs
@@ -0,0 +1,9 @@
+﻿namespace HelloWorld.Models.ViewModel
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gmail { get; set; }
+    }
+}
